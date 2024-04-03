03c9f2ed3043fa614c912055778682139c5241a3
Bryan Arredondo <bryanpool991@hotmail.com>
2024-04-03 12:33:39 -0500

    proyect userManagementSystem

---
+++ UserManagementSystem/ProfileManagement-Service/src/test/java/com/bryan/ProfileManagement_Service/ProfileManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.ProfileManagement_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
